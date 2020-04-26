8789156998829cd7322c33022d3be5f30a4c6d89
Victor JEMAN <victorjeman@gmail.com>
2020-04-26 20:33:29 +0300

    Update

--- components/ProjectTechnology/ProjectTechnology.style.js
+++ components/ProjectTechnology/ProjectTechnology.style.js
@@ -1,1 +1,14 @@
-export const ProjectTechnology = ({ technology }) => <div className="c-project-technology"></div>;
+import css from 'styled-jsx/css';
+
+import { MEDIA } from '../../common/settings/media.settings';
+
+export default css`
+  @media ${MEDIA.MOBILE} {
+  }
+
+  @media ${MEDIA.TABLET} {
+  }
+
+  @media ${MEDIA.DESKTOP} {
+  }
+`;
