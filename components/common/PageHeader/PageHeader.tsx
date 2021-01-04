f72d599fd50eda7762beca9379e1bf33c3509c08
Victor JEMAN <victorjeman@gmail.com>
2021-01-04 21:20:07 +0200

    Update

--- components/common/PageHeader/PageHeader.tsx
+++ components/common/PageHeader/PageHeader.tsx
@@ -8,6 +8,14 @@ export const PageHeader = (): JSX.Element => {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
 
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content="#A03232" />
+      <meta name="theme-color" content="#A03232" />
+
       <link
         href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500&family=Bebas+Neue&family=IBM+Plex+Sans:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400;1,500&family=Titillium+Web:wght@600&family=Merriweather:ital@0;1&display=swap"
         rel="stylesheet"
