b3f485626915cefd4515fe3353e2493c46247d35
Victor JEMAN <victorjeman@gmail.com>
2021-03-12 10:05:23 +0200

    Updates for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,21 @@ export default function Home(): React.ReactNode {
       <NextSeo
         title={'Victor JEMAN | Front-End Enthusiast'}
         description={'I do a little bit of web development and I love sharing my knowledge.'}
+        openGraph={{
+          url: 'https://victorjeman.com',
+          title: 'Victor JEMAN | Front-End Enthusiast',
+          description: 'I do a little bit of web development and I love sharing my knowledge.',
+          images: [
+            {
+              url: 'https://www.example.ie/og-image-01.jpg',
+              width: 970,
+              height: 634,
+              alt:
+                'A man writes hello at a keyboard with a cat to the left and a flower to the right',
+            },
+          ],
+          site_name: 'SiteName',
+        }}
       />
 
       <LandingHero />
