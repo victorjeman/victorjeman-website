d3eaf4fe734971d8b69c92dd5eeefa0f964be5d0
Victor JEMAN <victorjeman@gmail.com>
2021-03-18 10:49:11 +0200

    Update

--- components/common/Socials/Socials.tsx
+++ components/common/Socials/Socials.tsx
@@ -45,17 +45,15 @@ export const Socials = ({ shareUrl, title, sticky }: ISocials): JSX.Element => {
       </div>
 
       <ClientOnly>
-        <div className="c-socials__item">
-          <div
-            className="fb-like"
-            data-href={shareUrl}
-            data-width=""
-            data-layout="standard"
-            data-action="like"
-            data-size="small"
-            data-share="true"
-          ></div>
-        </div>
+        <div
+          className="fb-like"
+          data-href={shareUrl}
+          data-width=""
+          data-layout="box_count"
+          data-action="like"
+          data-size="small"
+          data-share="true"
+        ></div>
       </ClientOnly>
 
       <style jsx>{style}</style>
